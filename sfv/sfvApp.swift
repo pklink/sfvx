71f73c145a9b1ad08dcd4e8dbbe63bc7181729c8
Pierre Klink <dev@einself.net>
2025-07-12 20:16:18 +0200

    Initial Commit

---
+++ sfv/sfvApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfvApp.swift
+//  sfv
+//
+//  Created by Pierre Klink on 12.07.25.
+//
+
+import SwiftUI
+
+@main
+struct sfvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sfvTests/sfvTests.swift
@@ -0,0 +1,17 @@
+//
+//  sfvTests.swift
+//  sfvTests
+//
+//  Created by Pierre Klink on 12.07.25.
+//
+
+import Testing
+@testable import sfv
+
+struct sfvTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
